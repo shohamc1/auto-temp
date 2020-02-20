2bcd373e34af9ab3ac99c1f35e84a47d40b6ba6c
Shoham <shhmchk@gmail.com>
2020-02-20 10:44:13 +0800

    chamges

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def auto_temp():
         chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.binary_location = GOOGLE_CHROME_PATH
-        driver = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+        driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
     else:
         print('browser tag invalid')
 
