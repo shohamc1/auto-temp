04a0bef1843ba7b0f6f498b6d1f9b64ff334e4b4
Shoham <shhmchk@gmail.com>
2020-02-20 10:22:04 +0800

    lmasd

--- main.py
+++ main.py
@@ -27,7 +27,8 @@ def auto_temp():
     elif brow.lower() == "chrome":
         GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
         CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
-        chrome_options = webdriver.ChromeOptions()chrome_options.add_argument('--disable-gpu')
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.binary_location = GOOGLE_CHROME_PATH
         driver = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
