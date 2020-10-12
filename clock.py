1f846caafd15fb175f0a61472176ade5686833e3
Sean Chen <seancze@gmail.com>
2020-10-12 13:49:02 +0800

    Depend on jitterfix

--- clock.py
+++ clock.py
@@ -8,9 +8,9 @@ sched = BlockingScheduler()
 # start_time = time.time()
 
 # Run morning tasks - Daily Declaration + Record Temperature (Runs between 07:28 and 09:28)
-sched.add_job(temp_and_dec, 'cron', hour=8, minute=28, jitter=3600, misfire_grace_time=None, timezone="Singapore")
+sched.add_job(temp_and_dec, 'cron', hour=7, minute=28, jitter=3600, misfire_grace_time=None, timezone="Singapore")
 
 # Run afternoon task - Record Temperature (Runs between 17:38 and 19:38)
-sched.add_job(temp_only, 'cron', hour=18, minute=38, jitter=3600, misfire_grace_time=None, timezone="Singapore")
+sched.add_job(temp_only, 'cron', hour=17, minute=38, jitter=3600, misfire_grace_time=None, timezone="Singapore")
 
 sched.start()
